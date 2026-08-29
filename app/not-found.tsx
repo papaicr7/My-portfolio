e36dc903d2bee3e2c805496093882f66b13f691a
papaicr7 <dipyamandas96@gmail.com>
2026-08-29 14:31:34 +0530

    aaa

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-static';
+
 export default function NotFound() {
     return (
       <div className="flex flex-col items-center justify-center h-screen text-center px-4">
