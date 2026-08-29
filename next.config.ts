22cfc25a62341c6cce3e8e9f3008c8eb440f0861
papaicr7 <dipyamandas96@gmail.com>
2026-08-29 14:29:01 +0530

    WTF

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  typescript: {
-    // ignoreBuildErrors: true,
-  },
-};
-
-module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
-
 
 export default nextConfig;
